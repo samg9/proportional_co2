2dd84964b18007e7228076d6791f52febdfb94b4
samg9 <samgrewal.5@hotmail.com>
2021-03-28 00:11:27 -0700

    Working .todo: -co2 data, replace NaNs with Avg or just result to default. Could also scale more to show more effect.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import * as d3 from 'd3';
 import Cartogram from 'cartogram-chart';
 class App extends React.Component {
   getGDPPerCapita({ properties: p }) {
-    console.log(p,p.POP_EST, p.GDP_MD_EST );
-    return p.GDP_MD_EST * 1e6 / p.POP_EST;
+    // console.log(p,p.POP_EST, p.GDP_MD_EST );
+    var v = 0; 
+    try {
+      v = p[1999].co2*1e6;
+    }catch(err){
+      console.log(err)
+      v = 1e6;
+    }
+    return v;
 }
  constructor(props){
     super(props);
@@ -15,12 +22,9 @@ class App extends React.Component {
  componentDidMount(){
     console.log(this.myRef);
     const node  = this.myRef.current;
-    const myChart = Cartogram();
-    console.log(myChart);
-    d3.json('./ne_110m_admin_0_countries.json').then(world => {
-      console.log("incoming wolrdobjects....",world.objects);
+    d3.json('./countries_with_co2.json').then(world => {
       world.objects.countries.geometries.splice(
-        world.objects.countries.geometries.findIndex(d => d.properties.ISO_A2 === 'AQ'),
+        world.objects.countries.geometries.findIndex(d => d.properties.ISO_A2 === 'AQ'), //remove antartica?
         1
       );
 
--- src/datastuff.py
+++ src/datastuff.py
@@ -7,15 +7,37 @@ import os
 with open('/Users/sameer/code/proportional_co2/public/ne_110m_admin_0_countries.json') as jsondata:
 	data = json.load(jsondata)
 
-# using the from_dict load function. Note that the 'orient' parameter 
-#is not using the default value (or it will give the same error that you got before)
-# We transpose the resulting df and set index column as its index to get this result
-pd.DataFrame.from_dict(data, orient='index').T.set_index('index')   
-# list of countries
-geom = data['objects']['countries']['geometries'] 
-for i in geom:
-	print(i['properties']['NAME_CIAWF'])
-	
-afg = data['objects']['countries']['geometries'][0]['properties']['NAME_CIAWF']
-d = pd.read_csv('owid-co2-data.csv')
-afgg = data1.iloc[0]['country']
+d = pd.read_csv('/Users/sameer/code/proportional_co2/public/owid-co2-data.csv')
+# # let see how many countries co2 we have data for 
+years = list(range(1750,2020))
+
+for ix,row in d[['country','iso_code','co2','year']][:][pd.notnull(d.co2)][d.iso_code!=None][d.year.isin(years)].iterrows():
+	for i in range(len(data['objects']['countries']['geometries'] )):
+		possible_isos = [
+			data['objects']['countries']['geometries'][i]['properties']['ADM0_A3'], 
+			data['objects']['countries']['geometries'][i]['properties']['WB_A3'], 
+			data['objects']['countries']['geometries'][i]['properties']['ADM0_A3_IS'],
+			data['objects']['countries']['geometries'][i]['properties']['SOV_A3'],
+			data['objects']['countries']['geometries'][i]['properties']['GU_A3'],
+			data['objects']['countries']['geometries'][i]['properties']['SU_A3'],
+			data['objects']['countries']['geometries'][i]['properties']['BRK_A3'],
+			data['objects']['countries']['geometries'][i]['properties']['ISO_A3'],
+			data['objects']['countries']['geometries'][i]['properties']['ISO_A3_EH'],
+			data['objects']['countries']['geometries'][i]['properties']['ADM0_A3_IS'],
+			data['objects']['countries']['geometries'][i]['properties']['ADM0_A3_US'],
+		]
+		possible_country = [
+			data['objects']['countries']['geometries'][i]['properties']['SOVEREIGNT'],
+			data['objects']['countries']['geometries'][i]['properties']['ADMIN'],
+			data['objects']['countries']['geometries'][i]['properties']['GEOUNIT'], 
+			data['objects']['countries']['geometries'][i]['properties']['NAME'] ,
+			data['objects']['countries']['geometries'][i]['properties']['NAME_LONG'],
+			data['objects']['countries']['geometries'][i]['properties']['BRK_NAME'],
+			data['objects']['countries']['geometries'][i]['properties']['NAME_SORT'],
+		] 
+		if (row.country in possible_country) or (row.iso_code in possible_isos):
+			print("found a match for ", row.country)
+			data['objects']['countries']['geometries'][i]['properties'][str(row.year)] = {'co2': row.co2}
+
+with open('./countries_againandagain.json', 'w') as outfile:
+    json.dump(data, outfile)
